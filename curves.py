9550282ffa82f0f1ddb2b12691d612f149d2a891
Butaforsky <59537160+Butaforsky@users.noreply.github.com>
2023-04-15 10:03:02 +0300

    release 0.1.0

---
+++ curves.py
@@ -0,0 +1,45 @@
+max_delta = 0.008
+min_delta = 0.005
+
+l = 50
+
+base_point = 1337
+
+print("Расчет характерных точек для ландшафта\n")
+print("Автор: mgostev.it@gmail.com\n")
+print("Обо всех багах пишите на почту - буду фиксить")
+print("Git-Hub программки: ")
+
+while(base_point != 0):
+  
+  base_point = float(input("Первая точка: "))
+  if(base_point == 0):
+    exit()
+  xt_point = float(input("Вторая точка: "))
+  try:
+     l = float(input("Длина: "))
+  except: 
+     l = 50
+     pass
+  
+  delta = (base_point - xt_point)/l
+
+  if delta > max_delta:
+      print("Досыпаем")
+      xt_point = base_point - max_delta * l
+      print("Характерная точка: ", round(xt_point, 2))
+      delta = (base_point - xt_point)/l
+      print("Уклон:", round(delta*1000,2), "промилле \n")
+
+  elif delta < min_delta:
+      print("Убираем лишнее")
+      xt_point = base_point - min_delta * l
+      print("Характерная точка: ", round(xt_point, 2))
+      delta = (base_point - xt_point)/l
+      print("Уклон:", round(delta*1000,2), "промилле \n")
+
+  else:
+     print("Уклон в диапазоне 5 - 8 промилле")
+     print("Уклон: ", round(delta*1000,2), "промилле\n")
+
+        
