b21e0dbb4be5ad8530255694fb233357e430188d
Butaforsky <59537160+Butaforsky@users.noreply.github.com>
2023-04-15 13:05:08 +0300

    add export to csv

--- curves.py
+++ curves.py
@@ -1,7 +1,10 @@
-max_delta = 0.008
+import csv
+
+max_delta = 0.080
 min_delta = 0.005
 
 l = 50
+index = 1
 
 base_point = 1337
 
@@ -12,43 +15,52 @@ print("Git-Hub программки: https://github.com/Butaforsky/arc \n\n")
 
 print("Для выхода введите '0' (ноль)")
 
-while(base_point != 0):
-  
-  print("Введите высоты точек в метрах, используйте точку, а не запятую!")
-  print("По - умолчанию расстояние между точками равно 50 м\n")
-  
-  base_point = float(input("Первая точка, м: "))
-  if(base_point == 0):
-    exit()
-  xt_point = float(input("Вторая точка, м: "))
-  if(xt_point == 0):
-    exit()
-  try:
-     l = float(input("Длина, м: "))
-     if(l == 0):
-      exit()
-  except: 
-     l = 50
-     pass
-  
-  delta = (base_point - xt_point)/l
+with open('points.csv', mode='w', newline='') as csv_file:
+    fieldnames = ['Базовая точка', "Характерная точка", 'Уклон', 'Индекс']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
 
-  if delta > max_delta:
-      print("Досыпаем")
-      xt_point = base_point - max_delta * l
-      print("Характерная точка, м: ", round(xt_point, 2))
+    while(base_point != 0):
+      
+      print("Введите высоты точек в метрах, используйте точку, а не запятую!")
+      print("По - умолчанию расстояние между точками равно 50 м\n")
+      
+      base_point = float(input("Первая точка, м: "))
+      if(base_point == 0):
+        exit()
+      xt_point = float(input("Вторая точка, м: \n"))
+      if(xt_point == 0):
+        exit()
+      try:
+        l = float(input("Длина, м: "))
+        if(l == 0):
+          exit()
+      except: 
+        l = 50
+        pass
+      
       delta = (base_point - xt_point)/l
-      print("Уклон:", round(delta*1000,2), "промилле \n")
 
-  elif delta < min_delta:
-      print("Убираем лишнее")
-      xt_point = base_point - min_delta * l
-      print("Характерная точка, м: ", round(xt_point, 2))
-      delta = (base_point - xt_point)/l
-      print("Уклон:", round(delta*1000,2), "промилле \n")
+      if delta > max_delta:
+          print("Досыпаем")
+          xt_point = base_point - max_delta * l
+          print("Характерная точка, м: ", round(xt_point, 2))
+          delta = (base_point - xt_point)/l
+          print("Уклон:", round(delta*1000,2), "промилле \n")
 
-  else:
-     print("Уклон в диапазоне 5 - 8 промилле")
-     print("Уклон: ", round(delta*1000,2), "промилле\n")
+      elif delta < min_delta:
+          print("Убираем лишнее")
+          xt_point = base_point - min_delta * l
+          print("Характерная точка, м: ", round(xt_point, 2))
+          delta = (base_point - xt_point)/l
+          print("Уклон:", round(delta*1000,2), "промилле \n")
+
+      else:
+        print("Уклон в диапазоне 5 - 80 промилле")
+        print("Уклон: ", round(delta*1000,2), "промилле\n")
+      writer.writerow({'Базовая точка': base_point, 'Характерная точка': xt_point, 
+                        'Уклон': round(delta*1000,2), 'Индекс': index})
+
+      index+=1
 
         
